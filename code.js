5a5bc68483be70eabf097575e91fde846e5b45b3
Rinkesh Goyal <rinkeshgoyal95@gmail.com>
2021-06-15 14:05:21 +0530

    Javascript completed

---
+++ code.js
@@ -0,0 +1,71 @@
+let result=["rock","paper","scissor"];
+let playerScore=0;
+let computerScore=0;
+
+function computerPlay(){
+    return Math.floor(Math.random()*3);
+}
+
+
+function playRound(playerSelection,computerSelection){
+    if(playerSelection===computerSelection)
+        alert("Tie");
+    else if(playerSelection==="rock" && computerSelection === "scissor"){
+        alert("player"); 
+        playerScore++;
+    }
+    
+    else if(playerSelection==="scissor" && computerSelection === "rock"){ 
+        alert("computer");
+        computerScore++;
+    }
+
+    else if(playerSelection==="paper" && computerSelection === "scissor"){alert("computer"); computerScore++;}
+
+    else if(playerSelection==="scissor" && computerSelection === "paper"){        
+        alert("player"); 
+        playerScore++;
+    }
+
+    else if(playerSelection==="rock" && computerSelection === "paper"){alert("computer"); computerScore++;}
+
+    else{        
+        alert("player"); 
+        playerScore++;
+    }
+
+}
+
+
+
+function game(){
+    
+    for(let round = 0;round<5;round++){
+        let playerSelection=prompt("Enter your choice (Rock,Paper,Scissor)").toLowerCase();
+        let computerSelection=result[computerPlay()];
+        playRound(playerSelection,computerSelection);       
+    }
+
+    if(playerScore===computerScore) {
+        document.write(playerScore);
+        document.write(computerScore);
+    
+        alert("Tie")
+    }
+    else if(playerScore>computerScore){ 
+        document.write(playerScore);
+        document.write(computerScore);
+    
+        alert("You Won");
+    }
+    else{ 
+        document.write(playerScore);
+        document.write(computerScore);
+    
+        alert("You lose")
+    }
+}
+
+        
+game();
+
