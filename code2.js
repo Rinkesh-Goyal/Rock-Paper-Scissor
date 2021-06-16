4362f54d2be96695dc0b05aea063229914550590
Rinkesh Goyal <rinkeshgoyal95@gmail.com>
2021-06-16 17:30:37 +0530

    Rock Paper  Scissor completed

--- code2.js
+++ code2.js
@@ -1,50 +1,67 @@
-let input=["rock","paper","scissor"];
+let input=["Rock","Paper","Scissor"];
 let playerScore1=0;
 let computerScore1=0;
+let img=new Image();
+let playerSelection;
+let computerSelection;
 
 
 function computerPlay(){
     return Math.floor(Math.random()*3);
 }
 
-let playerSelection;
-let computerSelection=input[computerPlay()];
+playerSelection;
+computerSelection=input[computerPlay()];
 
 function playRound(playerSelection,computerSelection){
+    
     console.log(playerSelection)
+
     if(playerSelection===computerSelection){
-        console.log("Tie");
-        playerScore1=playerScore1;
-        computerScore1=computerScore1;
+       document.querySelector("#computerSelection").setAttribute("src",`Assets\\${computerSelection}.png`)
+       document.querySelector("#playerSelection").setAttribute("src",`Assets\\${playerSelection}.png`)
+        document.querySelector("#rslt").textContent="Tie";
     }
 
-    else if(playerSelection==="rock" && computerSelection === "scissor"){
-        console.log("player"); 
+    else if(playerSelection==="Rock" && computerSelection === "Scissor"){
+        document.querySelector("#computerSelection").setAttribute("src",`Assets\\${computerSelection}.png`)
+       document.querySelector("#playerSelection").setAttribute("src",`Assets\\${playerSelection}.png`)
+        document.querySelector("#rslt").textContent="You Won";
         playerScore1++;
     }
     
-    else if(playerSelection==="scissor" && computerSelection === "rock"){ 
-        console.log("computer");
+    else if(playerSelection==="Scissor" && computerSelection === "Rock"){ 
+        document.querySelector("#computerSelection").setAttribute("src",`Assets\\${computerSelection}.png`)
+       document.querySelector("#playerSelection").setAttribute("src",`Assets\\${playerSelection}.png`)
+        document.querySelector("#rslt").textContent="You lose!";
         computerScore1++;
     }
 
-    else if(playerSelection==="paper" && computerSelection === "scissor"){
-        console.log("computer"); 
+    else if(playerSelection==="Paper" && computerSelection === "Scissor"){
+        document.querySelector("#computerSelection").setAttribute("src",`Assets\\${computerSelection}.png`)
+       document.querySelector("#playerSelection").setAttribute("src",`Assets\\${playerSelection}.png`)
+        document.querySelector("#rslt").textContent="You lose!";
         computerScore1++;
     }
 
-    else if(playerSelection==="scissor" && computerSelection === "paper"){        
-        console.log("player"); 
+    else if(playerSelection==="Scissor" && computerSelection === "Paper"){        
+        document.querySelector("#computerSelection").setAttribute("src",`Assets\\${computerSelection}.png`)
+       document.querySelector("#playerSelection").setAttribute("src",`Assets\\${playerSelection}.png`)
+        document.querySelector("#rslt").textContent="You Won";
         playerScore1++;
     }
 
-    else if(playerSelection==="rock" && computerSelection === "paper"){
-        console.log("computer");
+    else if(playerSelection==="Rock" && computerSelection === "Paper"){
+        document.querySelector("#computerSelection").setAttribute("src",`Assets\\${computerSelection}.png`)
+       document.querySelector("#playerSelection").setAttribute("src",`Assets\\${playerSelection}.png`)
+        document.querySelector("#rslt").textContent="You lose!";
         computerScore1++;
     }
 
     else{        
-        console.log("player"); 
+        document.querySelector("#computerSelection").setAttribute("src",`Assets\\${computerSelection}.png`)
+       document.querySelector("#playerSelection").setAttribute("src",`Assets\\${playerSelection}.png`)
+        document.querySelector("#rslt").textContent="You Won";
         playerScore1++;
     }
 
@@ -60,7 +77,7 @@ function playRound(playerSelection,computerSelection){
 // }
 
 function getPlayerSelection(e){
-    playerSelection=e.target.value.toLowerCase();
+    playerSelection=e.target.id;
     return playerSelection;
 }
 
@@ -77,8 +94,8 @@ function game(e){
     
     playRound(getPlayerSelection(e),computerSelection);       
     
-    console.log(playerScore1);
-    console.log(computerScore1);
+    document.querySelector("#playerScore").innerHTML=playerScore1.toString();
+    document.querySelector("#computerScore").innerHTML=computerScore1.toString();
 
 
     if(playerScore1===5||computerScore1===5){
